7da887c07dcdbfacdb86c98be034f60d8da4d52f
Spark <spark.scul@gmail.com>
2022-04-24 22:02:44 -0400

    Trim unused code, separate row lookup generation into new file

---
+++ generateRowLookup.py
@@ -0,0 +1,89 @@
+
+
+# Generate all possible rows
+def genAllRows():
+	allRows = []
+	allCells = [G,Y,B]
+	for t0 in allCells:
+		for t1 in allCells:
+			for t2 in allCells:
+				for t3 in allCells:
+					for t4 in allCells:
+						allRows.append([t0,t1,t2,t3,t4])
+	return allRows
+
+
+# Score a guess. Given an answer and guess, return the row
+def scoreGuess(answer, guess):
+	answer = list(answer)
+	guess = list(guess)
+	row = [0,0,0,0,0]
+	lettersLeft = answer.copy()
+	# Figure out greens
+	for letterIndex,letter in enumerate(guess):
+		if guess[letterIndex] == answer[letterIndex]:
+			row[letterIndex] = 2
+			lettersLeft.remove(guess[letterIndex])
+	# Figure out yellows
+	for letterIndex,letter in enumerate(guess):
+		if row[letterIndex] == 2:
+			continue
+		elif guess[letterIndex] in lettersLeft:
+			row[letterIndex] = 1;
+			lettersLeft.remove(guess[letterIndex])
+	return row
+
+# given a row and a word, return only the green letters, with ?'s for the
+# rest of the letters
+greenCache = defaultdict(list)
+def greenKey(row,answer):
+	k = ""
+	for i, color in enumerate(row):
+		if color == G:
+			k += answer[i]
+		else:
+			k += "?"
+	return k
+
+# print(greenKey([2,2,0,0,2], "TESTS"))
+
+# Make a list of all combinations of greens
+# This is just enumerating binary numbers for 5 bits (green/black)
+greenRows = []
+for i in range(32):
+	greenRows.append([G if ((1 << bit) & i) != 0 else B for bit in range(5)])
+# print(greenRows)
+
+for word in wordlist:
+	for row in greenRows:
+		greenCache[greenKey(row,word)].append(word)
+
+
+# For a given row, check if the given answer could 
+# have produced that row for any guess
+def validAnswer(row, answer, dictionary):
+	for word in greenCache[greenKey(row,answer)]:
+		if row == scoreGuess(answer, word): 
+			# answer has at least one valid guess for this row
+			return True
+	# answer has no valid guesses. Trim answer from subDictionary
+	return False
+
+# Generate row lookup table
+# For every possible row, generate a list of every possble answer that could result in that row
+def generateRowLookup(dictionary):
+	rowLookup = defaultdict(list)
+
+	f = open("rowLookupTable.py", 'w')
+	allRows = genAllRows()
+	for row in allRows:
+		strRow = ''.join(str(i) for i in row) # Convert list of ints to str
+		rowLookup[strRow] = []
+		for answer in dictionary:
+			if validAnswer(row, answer, dictionary):
+				rowLookup[strRow].append(answer)
+		print(strRow)
+	f.write("rowLookup = defaultdict(list)")
+	f.write("rowLookup.update(" + json.dumps(rowLookup) + ')\n')
+	f.close()
+	return rowLookup
--- wordlr.py
+++ wordlr.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import copy
 import datetime
 import json
@@ -13,256 +14,18 @@ WIN_GAP = 20 # How much of a lead one answer needs to achieve before declaring v
 
 ANSWER_CHECK = '' # Debug: Put correct answer here to flag erroneous strikes
 
-# rows = parseGram("""⬛Yellow squareGreen square⬛⬛
-# Green square⬛Green squareGreen squareGreen square
-# Green squareGreen squareGreen squareGreen squareGreen square
-# ⬜⬜Yellow square⬜Yellow square
-# Green squareYellow square⬜⬜⬜
-# Green square⬜⬜Green square⬜
-# Green square⬜Green squareGreen squareGreen square
-# Green squareGreen squareGreen squareGreen squareGreen square
-# ⬛Yellow square⬛Green square⬛
-# ⬛⬛Green squareGreen square⬛
-# ⬛⬛Green squareGreen square⬛
-# ⬛⬛Green squareGreen squareGreen square
-# Green square⬛Green squareGreen squareGreen square
-# ⬜Yellow square⬜⬜Yellow square
-# Yellow square⬜⬜Green squareGreen square
-# Green squareGreen squareGreen squareGreen squareGreen square
-# ⬛Yellow square⬛⬛⬛
-# ⬛⬛⬛Green square⬛
-# ⬛⬛⬛Yellow square⬛
-# Green squareYellow square⬛⬛⬛
-# Green squareYellow squareGreen square⬛Yellow square
-# Green square⬜Green squareGreen square⬜
-# Green square⬜Green squareGreen squareGreen square
-# Green square⬛⬛⬛⬛
-# Green square⬛Green square⬛⬛
-# Green squareGreen squareGreen square⬛⬛
-# ⬛⬛⬛⬛⬛
-# ⬛⬛Green square⬛Yellow square
-# ⬛Yellow squareGreen square⬛⬛
-# Green square⬛Green squareGreen square⬛
-# Green square⬛Green squareGreen squareGreen square
-# ⬛Yellow square⬛⬛⬛
-# Green squareYellow squareYellow squareGreen square⬛
-# Green square⬛Green squareGreen squareGreen square
-# ⬛Yellow square⬛⬛⬛
-# ⬛⬛Yellow squareGreen square⬛
-# ⬛Yellow squareYellow square⬛Yellow square
-# Green square⬛Green squareGreen squareGreen square
-# ⬜⬜⬜⬜⬜
-# ⬜Yellow square⬜⬜⬜
-# Yellow square⬜⬜⬜⬜
-# ⬜⬜⬜Yellow square⬜
-# Green square⬜Green square⬜⬜
-# Green squareYellow square⬛⬛⬛
-# Green squareYellow square⬛⬛Green square
-# Green square⬛Green squareGreen squareGreen square
-# ⬛⬛Green square⬛⬛
-# ⬛⬛Green squareGreen squareGreen square
-# ⬛⬛Green squareGreen squareGreen square
-# Green square⬛Green squareGreen squareGreen square
-# Yellow square⬛⬛⬛⬛
-# ⬛⬛⬛⬛⬛
-# Green square⬛⬛Green square⬛
-# ⬛⬛⬛Yellow square⬛
-# ⬛Yellow squareYellow square⬛Yellow square
-# Green squareYellow squareGreen square⬛Yellow square
-# ⬛⬛⬛Yellow square⬛
-# ⬛⬛Green square⬛⬛
-# Green squareYellow squareGreen square⬛⬛
-# ⬜⬜⬜Yellow square⬜
-# Green square⬜Green square⬜Yellow square
-# ⬜⬜⬜Yellow square⬜
-# Green square⬜Green square⬜Yellow square
-# ⬛⬛Yellow squareGreen square⬛
-# ⬛Yellow squareYellow square⬛Yellow square
-# ⬛⬛⬛⬛Yellow square
-# Green square⬛⬛⬛Yellow square
-# Green squareGreen square⬛⬛⬛
-# Green squareGreen squareGreen square⬛⬛
-# ⬜Yellow square⬜⬜Yellow square
-# Yellow square⬜⬜Green squareGreen square""")
-
-
-
-# rows = [[G,B,G,G,G],[G,Y,Y,Y,B],[G,G,G,B,B],[G,B,B,G,B],[B,B,B,B,Y],[Y,Y,B,B,B],[G,B,G,B,Y],[G,B,B,Y,G],[G,B,B,B,Y],[B,B,G,G,Y],[B,B,B,Y,B],[B,B,G,B,Y],[B,Y,G,Y,B],[B,B,G,G,B],[G,Y,G,B,Y],[G,Y,G,B,Y],[Y,Y,B,B,B]] + rows
-from collections import defaultdict
-
-# Generate all possible rows
-def genAllRows():
-	allRows = []
-	allCells = [G,Y,B]
-	for t0 in allCells:
-		for t1 in allCells:
-			for t2 in allCells:
-				for t3 in allCells:
-					for t4 in allCells:
-						allRows.append([t0,t1,t2,t3,t4])
-	return allRows
-
-
-
-# Convert a pasted wordle gram to a list of rows
-def parseGram(gram):
-	g = re.sub("[a-z ]","",gram)
-	rows = []
-	for gramRow in g.split('\n'):
-		row = []
-		for block in gramRow:
-			if block == "Y":
-				row.append(1)
-			elif block == "G":
-				row.append(2)
-			elif block == "⬛" or block == "⬜":
-				row.append(0)
-			else:
-				raise Exception("Ohno: " + block )
-		rows.append(row)
-	return rows
-
+# Load the list of all viable Wordle words
 with open('wordList.txt') as f:
     wordlist = [ line.strip() for line in f ]
-# 
+
+# Shorthand constants for block colors
 Y = 1
 G = 2 
 B = 0
+
+# Figure out today's Wordle number
 wordleEpoch = datetime.date(2021, 6, 19)
 wordleNumberToday = (datetime.date.today() - wordleEpoch).days
-
-greenCache = defaultdict(list)
-
-# given a row and a word, return only the green letters, with ?'s for the
-# rest of the letters
-def greenKey(row,answer):
-	k = ""
-	for i, color in enumerate(row):
-		if color == G:
-			k += answer[i]
-		else:
-			k += "?"
-	return k
-
-# print(greenKey([2,2,0,0,2], "TESTS"))
-
-# Make a list of all combinations of greens
-# This is just enumerating binary numbers for 5 bits (green/black)
-greenRows = []
-for i in range(32):
-	greenRows.append([G if ((1 << bit) & i) != 0 else B for bit in range(5)])
-# print(greenRows)
-
-for word in wordlist:
-	for row in greenRows:
-		greenCache[greenKey(row,word)].append(word)
-
-
-
-# Score a guess. Given an answer and guess, return the row
-def scoreGuess(answer, guess):
-	answer = list(answer)
-	guess = list(guess)
-	row = [0,0,0,0,0]
-	lettersLeft = answer.copy()
-	# Figure out greens
-	for letterIndex,letter in enumerate(guess):
-		if guess[letterIndex] == answer[letterIndex]:
-			row[letterIndex] = 2
-			lettersLeft.remove(guess[letterIndex])
-	# Figure out yellows
-	for letterIndex,letter in enumerate(guess):
-		if row[letterIndex] == 2:
-			continue
-		elif guess[letterIndex] in lettersLeft:
-			row[letterIndex] = 1;
-			lettersLeft.remove(guess[letterIndex])
-	return row
-
-# For a given row, check if the given answer could 
-# have produced that row for any guess
-def validAnswer(row, answer, dictionary):
-	for word in greenCache[greenKey(row,answer)]:
-		if row == scoreGuess(answer, word): 
-			# answer has at least one valid guess for this row
-			return True
-	# answer has no valid guesses. Trim answer from subDictionary
-	return False
-
-def checkAnswer(answer, tweets, rowLookup, wordleNumberToday):
-	renderedTweets = parseTweets(tweets, wordleNumberToday)
-	for i, renderedTweet in enumerate(renderedTweets):
-		for j, row in enumerate(renderedTweet):
-			if not answer in rowLookup[''.join(str(i) for i in row)]:
-			# if validAnswer(row, answer, dictionary) == False:
-				print(f"Row {j} in Tweet {i} is invalid. Tweet: ")
-				print(tweets[i].split('\n'))
-				print("Rendered tweet: ")
-				print(renderedTweet)
-
-# Generate row lookup table
-# For every possible row, generate a list of every possble answer that could result in that row
-def generateRowLookup(dictionary):
-	rowLookup = defaultdict(list)
-
-	f = open("rowLookupTable.py", 'w')
-	allRows = genAllRows()
-	for row in allRows:
-		strRow = ''.join(str(i) for i in row) # Convert list of ints to str
-		rowLookup[strRow] = []
-		for answer in dictionary:
-			if validAnswer(row, answer, dictionary):
-				rowLookup[strRow].append(answer)
-		print(strRow)
-	f.write("rowLookup = defaultdict(list)")
-	f.write("rowLookup.update(" + json.dumps(rowLookup) + ')\n')
-	f.close()
-	return rowLookup
-
-# Take a row and trim down the dictionary
-# Go through dictionary
-# For each word, assume it's the answer
-# check that there is at least one other word in the dictionary that would 
-# result in the given score
-def trimDictionary(row, subDictionary, dictionary):
-	for answer in subDictionary:
-		if not validAnswer(row, answer, dictionary):
-			str1 = ''
-			# print("Trimming ", str1.join(answer))
-			subDictionary.remove(str1.join(answer))
-
-
-# Trim the dictionary based on a list of rows
-def trimDictionaryRows(rows, subDictionary, dictionary):
-	for row in rows:
-		trimDictionary(row, subDictionary, dictionary)
-		print("trimmed down to ",len(subDictionary))
-		# if len(subDictionary) == 1:
-		# 	print(subDictionary[0])
-		# 	return subDictionary
-	return subDictionary
-
-
-# Trim the dictionary based on a list of rendered tweets
-# renderedTweets should be a list of lists of rows
-def trimDictionaryRenderedTweets(renderedTweets, subDictionary, dictionary):
-	for i, renderedTweet in enumerate(renderedTweets):
-		trimDictionaryRows(renderedTweet, subDictionary, dictionary)
-		# if len(subDictionary) == 1:
-		# 	return subDictionary
-	return subDictionary
-
-# Takes a row, and increments the count for each 
-# word of a tally dictionary that doesn't fit the row
-# tallyDictionary input should be a dict
-# key is word, value is tally
-# dictionary is the list of words to validate rows
-def tallyRowStrikes(row, tallyDictionary, dictionary):
-	for word in list(tallyDictionary.keys()):
-		if not validAnswer(row, word, dictionary):
-			tallyDictionary[word] += 1
-
-
 
 # Takes a parsed Tweet (list of rows)
 # Check duplicate rows:
@@ -287,12 +50,6 @@ def tallyTweetStrikes(tweet, tallyDictionary, rowLookup):
 				# delete guess from scratchRowLookup entry for this row
 				# This ensures that duplicate rows must be reachable from multiple guesses for each answer
 				scratchRowLookup[rowStr].remove(answer)
-
-
-
-##################################################################################
-##################################################################################
-##################################################################################
 
 def sortDict(inDict, topWords):
 	for word in list(inDict.keys()):
@@ -329,8 +86,6 @@ def tallyStrikes(tallyDictionary, renderedTweets, dictionary, rowLookup, lineCou
 		for row in tweet:
 			lineCount += 1
 			tallyRowStrikesFast(row, tallyDictionary, rowLookup)
-			# tallyRowStrikes(row, tallyDictionary, dictionary, rowLookup) # Use old dict looping method
-			# tallyRowStrikes = sorted(tallyRowStrikes, key=tallyRowStrikes.get) # sort by strikes
 		# tallyTweetStrikes(tweet, tallyDictionary, rowLookup) # try new method with checks for duplicates
 		topWords = sortDict(tallyDictionary, topWords)
 		print(topWords[0], ":", tallyDictionary[topWords[0]], " second place:", topWords[1],":",tallyDictionary[topWords[1]], " Remaining:", len(tallyDictionary), "Processed ", lineCount, " lines")
@@ -440,15 +195,5 @@ if __name__ == '__main__':
 	print(f"{topWords[0]} wins with {tallyDictionary[topWords[0]]}")
 	print(f"{topWords[1]} in second with {tallyDictionary[topWords[1]]}")
 	print(f"Parsed {lineCount} lines")
-
-
-# # Run with trimming method
-# subDictionary = dictionary.copy()
-# subDictionary = trimDictionaryRenderedTweets(renderedTweets, subDictionary, dictionary)
-# while len(subDictionary) != 1:
-# 	renderedTweets = parseTweets(tweets, wordleNumberToday)
-# 	subDictionary = trimDictionaryRenderedTweets(renderedTweets, subDictionary, dictionary)
-# 	print(subDictionary)
-# 	sleep(30)
 
 
